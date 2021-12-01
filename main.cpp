9bae70c5d3d20103ccd8d0779a4d5a770549fa78
AlessandroMeschi <91841733+AlessandroMeschi@users.noreply.github.com>
2021-12-01 22:15:06 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.h
+using namespace std;
 
 int main(){
+  int N=0, b=N-1;
+  cin >> N;
+  if(numeriprimi(N,b)==true){
+    cout << "numero primo" <<endl;
+  }else{
+    cout << "numero non primo" <<endl;
+  }
   return 0;
 }
